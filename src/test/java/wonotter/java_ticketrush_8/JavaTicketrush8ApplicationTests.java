105f62856c358afec233a02d3bbdc7c28679b095
wonotter <zjaxhfl99@kumoh.ac.kr>
2025-11-12 21:13:03 +0900

    feat: setup project

---
+++ src/test/java/wonotter/java_ticketrush_8/JavaTicketrush8ApplicationTests.java
@@ -0,0 +1,13 @@
+package wonotter.java_ticketrush_8;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaTicketrush8ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
